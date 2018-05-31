72c590248daf9181488bbde0a0b44d7b1b5bc5d7
turntayble81 <turntayble81@gmail.com>
2018-05-30 22:47:57 -0400

    Got everything working

---
+++ config.js
@@ -0,0 +1,12 @@
+
+//Set the following properties for the
+//recordset which needs to be updated
+
+const config = {
+    recordsetName     : 'example.com',
+    recordsetTTL      : 300,
+    recordsetComment  : 'Some comment about record set',
+    hostedZoneId      : 'Z8W873JNKE8S'
+};
+
+module.exports = config;
--- route53Updater.js
+++ route53Updater.js
@@ -1,4 +1,5 @@
 const async  = require('async');
+const fs     = require('fs');
 const AWS    = require('aws-sdk');
 const config = require('./config');
 const {exec} = require('child_process');
@@ -7,50 +8,95 @@ const route53 = new AWS.Route53({
     apiVersion: '2013-04-01'
 });
 
-//TODO: Write last successfully set IP to file
-//TODO: Check if ip has changed from file.
-    //- If yes or file does not exist, update route53
-    //- If no, do nothing
+const IP_FILE = `${__dirname}/.lastRecordsetIp`;
 
-async.waterfall([
-    (next) => {
-        exec('curl -s ipinfo.io/ip', next);
-    },
-    (ip, stderr, next) => {
+async.parallel([
+    (pNext) => getCurrentIp(pNext),
+    (pNext) => getLastRecordSetIp(pNext)
+], (err, result) => {
+    if(err) {
+        return console.error(err);
+    }
+    let currentIp       = result[0];
+    let lastRecordSetIp = result[1];
+
+    currentIp       = sanitizeIP(currentIp);
+    lastRecordSetIp = sanitizeIP(lastRecordSetIp);
+
+    console.log(`Current IP    : ${currentIp}`);
+    console.log(`Record set IP : ${lastRecordSetIp || 'unknown'}`);
+
+    if(currentIp == lastRecordSetIp) {
+        console.log('Record set IP is already up to date');
+    }else {
+        console.log('Updating record set');
+        updateRecordSet(currentIp, (err) => {
+            if(err) {
+                console.error(err);
+            }else {
+                console.log('Successfully updated record set');
+            }
+        });
+    }
+});
+
+function sanitizeIP(ip) {
+    if(!ip) {
+        return ip;
+    }
+    return ip.replace(/[^0-9.]/g, '');
+}
+function getCurrentIp(cb) {
+    console.log('Determining current IP');
+    exec('curl -s ipinfo.io/ip', (err, stdout, stderr) => {
+        if(err) {
+            return cb(err);
+        }
         if(stderr) {
             console.error(stderr);
         }
-        console.log('**', ip)
-        next();
-    }
-], (err) => {
-    if(err) {
-        console.error(err);
-    }else {
-        console.log('Update successful');
-    }
-});
-/*
-const params = {
-    ChangeBatch: {
-        Changes: [{
-            Action: "UPSERT", 
-            ResourceRecordSet: {
-                Name: "haigo.org",
-                ResourceRecords: [{
-                    Value: "74.199.55.152"
-                }], 
-                TTL: 300,
-                Type: "A"
+        cb(null, stdout);
+    });
+}
+
+function getLastRecordSetIp(cb) {
+    console.log('Determining last known record set IP');
+    fs.stat(IP_FILE, (err, stat) => {
+        if(err) {
+            if(err.code == 'ENOENT') {
+                //file does not exist
+                cb(null, null);
+            }else {
+                cb(err);
             }
-        }],
-        Comment: "DNS for ITX Server"
-    }, 
-    HostedZoneId: "Z3EVZUL5N5WGQ7"
-};
+            return;
+        }
 
-route53.changeResourceRecordSets(params, (err, data) => {
+        fs.readFile(IP_FILE, {encoding: 'utf8'}, cb);
+    });
+}
 
-});
+function updateRecordSet(ip, cb) {
+    const params = {
+        ChangeBatch: {
+            Changes: [{
+                Action: "UPSERT", 
+                ResourceRecordSet: {
+                    Name: config.recordsetName,
+                    ResourceRecords: [{
+                        Value: ip
+                    }], 
+                    TTL: config.recordsetTTL,
+                    Type: "A"
+                }
+            }],
+            Comment: config.recordsetComment
+        }, 
+        HostedZoneId: config.hostedZoneId
+    };
 
-*/
+    async.series([
+        (next) => route53.changeResourceRecordSets(params, next),
+        (next) => fs.writeFile(IP_FILE, ip, next)
+    ], cb);
+}
